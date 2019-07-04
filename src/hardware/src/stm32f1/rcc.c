b75b25fc7ecf4e0657510114eddc573ef035367a
pokrov <al.n.pokrovsky@gmail.com>
2019-07-04 14:53:54 +0300

    fix rcc for 16MHz

--- src/hardware/src/stm32f1/rcc.c
+++ src/hardware/src/stm32f1/rcc.c
@@ -5,8 +5,8 @@
 
 
 void rcc_init() {
-	rcc_clock_setup_in_hse_8mhz_out_24mhz();
-	rcc_set_ppre2(RCC_CFGR_PPRE2_HCLK_DIV2);
+	//rcc_clock_setup_in_hse_8mhz_out_24mhz();
+	rcc_clock_setup_in_hse_16mhz_out_72mhz();
 }
 
 
