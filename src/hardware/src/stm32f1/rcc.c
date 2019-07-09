9882f31b12929fa8fede28f1d250012ec6dd8a88
pokrov <al.n.pokrovsky@gmail.com>
2019-07-09 14:32:16 +0300

    rcc configurable

--- src/hardware/src/stm32f1/rcc.c
+++ src/hardware/src/stm32f1/rcc.c
@@ -1,13 +1,19 @@
 #if defined(STM32F1)
 
 #include "rcc.h"
+#include "DeviceConfig.h"
 #include <libopencm3/stm32/rcc.h>
 
 
 void rcc_init() {
+#if HSE_Value == 8000000
 	rcc_clock_setup_in_hse_8mhz_out_72mhz();
 	//rcc_clock_setup_in_hse_8mhz_out_24mhz();
-	//rcc_clock_setup_in_hse_16mhz_out_72mhz();
+#elif HSE_Value == 16000000
+	rcc_clock_setup_in_hse_16mhz_out_72mhz();
+#else
+#error "HSE value not defined"
+#endif
 }
 
 
