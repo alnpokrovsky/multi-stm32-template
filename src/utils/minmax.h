7d7e9353434b8ba7b629686e9f5c88e3c5fef5a7
pokrov <al.n.pokrovsky@gmail.com>
2019-07-23 15:26:20 +0300

    fix max

--- src/utils/minmax.h
+++ src/utils/minmax.h
@@ -1,7 +1,7 @@
 #ifndef __UTILS_MIN_MAX_H__
 #define __UTILS_MIN_MAX_H__
 
-#define MAX(a,b) (((a) (b)) ? (a) : (b))
+#define MAX(a,b) (((a) > (b)) ? (a) : (b))
 #define MIN(a,b) (((a) < (b)) ? (a) : (b))
 
 #endif
