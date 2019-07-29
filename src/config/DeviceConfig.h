679e73efa9354c619454376ed84c9e65bc9c1417
pokrov <al.n.pokrovsky@gmail.com>
2019-07-29 14:05:07 +0300

    unite stm32 config

--- src/config/DeviceConfig.h
+++ src/config/DeviceConfig.h
@@ -1,10 +1,8 @@
 #ifndef __DEVICE_CONFIG_H__
 #define __DEVICE_CONFIG_H__
 
-#if defined(STM32F1)
-#   include "device/STM32F103C8t_config.h"
-#elif defined(STM32F4)
-#   include "device/STM32F4DISCO_config.h"
+#if defined(STM32F1) || defined(STM32F3) || defined(STM32F4)
+#   include "device/STM32_config.h"
 #elif defined(USE_MDR1986VE9x)
 #   include "device/MDR32F9Qx_config.h"
 #else
--- src/config/FreeModbusConfig.h
+++ src/config/FreeModbusConfig.h
@@ -5,12 +5,12 @@
 #define MODBUS_TIM               TIM_2
 #define MODBUS_TIM_HANDLER()     tim2_handler()
 
-#define MODBUS_UART              UART_2
-#define MODBUS_UART_RX_HANDLER() uart2_rx_handler()
-#define MODBUS_UART_TX_HANDLER() uart2_tx_handler()
-#define MODBUS_UART_TC_HANDLER() uart2_tc_handler()
+#define MODBUS_UART              UART_1
+#define MODBUS_UART_RX_HANDLER() uart1_rx_handler()
+#define MODBUS_UART_TX_HANDLER() uart1_tx_handler()
+#define MODBUS_UART_TC_HANDLER() uart1_tc_handler()
 
-#define RS485_TXE_PIN            PA_4
+#define RS485_TXE_PIN            PA_8
 
 #define MODBUS_TABLE_HREG_SIZE   10
 #define MODBUS_TABLE_IREG_SIZE   10
