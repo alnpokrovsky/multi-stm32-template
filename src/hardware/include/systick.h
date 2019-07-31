51ea403275a0de22161bcc00e5177affb6cdacd8
pokrov <al.n.pokrovsky@gmail.com>
2019-07-31 18:13:07 +0300

    improve systick interface

--- src/hardware/include/systick.h
+++ src/hardware/include/systick.h
@@ -3,12 +3,17 @@
 
 #include <stdint.h>
 
+typedef enum {
+    MILLISEC,
+    MICROSEC,
+} SYSTICK_UNITS;
+
 /**
  * init systick timer and start it
  * release 'void sys_tick_handler(void)' from nvic.h
  * to enter interrupt
  */
-void systick_start_interrupt(uint32_t ticks);
+void systick_start_interrupt(uint32_t ticks, SYSTICK_UNITS units);
 
 void systick_handler(void);
 
--- src/hardware/src/stm32/systick.c
+++ src/hardware/src/stm32/systick.c
@@ -2,6 +2,7 @@
 
 #include "systick.h"
 #include <libopencm3/cm3/systick.h>
+#include <libopencm3/stm32/rcc.h>
 #include <libopencm3/cm3/nvic.h>
 
 static void null_handler(void)
@@ -11,10 +12,18 @@ static void null_handler(void)
 
 #pragma weak systick_handler = null_handler
 
-void systick_start_interrupt(uint32_t ticks) {
+static const uint32_t PRESCAL_UNITS[] = {
+    10000,    /* MILLISEC */
+    1000000,  /* MICROSEC */
+};
+
+void systick_start_interrupt(uint32_t ticks, SYSTICK_UNITS units) {
     systick_set_clocksource(STK_CSR_CLKSOURCE_AHB_DIV8);
+
+    uint32_t prescal = rcc_ahb_frequency / PRESCAL_UNITS[units];
+
 	/* SysTick interrupt every N clock pulses: set reload to N-1 */
-	systick_set_reload(ticks);
+	systick_set_reload(prescal * ticks);
 	systick_interrupt_enable();
 	/* start */
 	systick_counter_enable();
